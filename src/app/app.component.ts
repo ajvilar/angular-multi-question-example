f62307ff5d83ecb1441f89806fe658df31c9a393
alfonso vilar <AJ@Jennifers-iMac.fios-router.home>
2021-06-21 15:42:38 -0400

    saving working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,5 @@
 import { Component, VERSION } from '@angular/core';
 
-import { IOptions, IQuestionInfo } from './components/multi-question/multi-question.component';
-  
-const yesNoOptions: Array<IOptions> = [
-  { key: 'Y', value: 'Yes' },
-  { key: 'N', value: 'No' }
-];
-
-const yesNoDont_KnownOptions: Array<IOptions> = [
-  { key: 'Y', value: 'Yes' },
-  { key: 'N', value: 'No' },
-  { key: 'U', value: "Don't Know" },
-];
 
 @Component({
   selector: 'app-root',
@@ -21,81 +9,5 @@ const yesNoDont_KnownOptions: Array<IOptions> = [
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  public questions: Array<IQuestionInfo> = [
-    {
-      key: '1', text: 'first question', choice: '', options: yesNoOptions, show: true,
-      rules: {always: true}
-    },
-    {
-      key: '2', text: 'second question', choice: '', options: yesNoDont_KnownOptions, show: true,
-      rules: {
-        dependsOn: ['1'], // only show this question if the following are visible
-        choiceRules: {
-          '1': ['N'],  // only show if question '3' has a value of 'N'
-        },
-      }
-    },
-    {
-      key: '3', text: 'third question', choice: '', options: yesNoOptions, show: true,
-      rules: {
-        dependsOn: ['2'], // only show this question if the following are visible
-        choiceRules: {
-          '2': ['Y', 'U'],  // only show if question '3' has a value of 'N'
-        },
-      }
-    },
-    {
-      key: '4',
-      text: 'fourth question', 
-      choice: '', 
-      options: yesNoOptions,
-      rules: {
-        dependsOn: ['3'], // only show this question if the following are visible
-        choiceRules: {
-          '3': ['N'],  // only show if question '3' has a value of 'N'
-        },
-      },
-      show: true
-    }
-  ];
 
-  public questions2: Array<IQuestionInfo> = [
-    {
-      key: '1', text: 'first question', choice: '', options: yesNoOptions, show: true,
-      rules: {always: true}
-    },
-    {
-      key: '2', text: 'second question', choice: '', options: yesNoDont_KnownOptions, show: true,
-      rules: {
-        dependsOn: ['1'], // only show this question if the following are visible
-        choiceRules: {
-          '1': ['N'],  // only show if question '3' has a value of 'N'
-        },
-      }
-    },
-    {
-      key: '3', text: 'third question', choice: '', options: yesNoOptions, show: true,
-      rules: {
-        dependsOn: ['2'], // only show this question if the following are visible
-        choiceRules: {
-          '2': ['Y', 'U'],  // only show if question '3' has a value of 'N'
-        },
-      }
-    },
-    {
-      key: '4',
-      text: 'fourth question', 
-      choice: '', 
-      options: yesNoOptions,
-      rules: {
-        dependsOn: ['3'], // only show this question if the following are visible
-        choiceRules: {
-          '3': ['N'],  // only show if question '3' has a value of 'N'
-        },
-      },
-      show: true
-    }
-  ];
-
-  public saveHiddenValues: boolean = false;
 }
--- src/app/components/multi-question/multi-question.component.ts
+++ src/app/components/multi-question/multi-question.component.ts
@@ -1,17 +1,31 @@
 import { Component, Input, OnInit, VERSION } from '@angular/core';
 
-export interface IOptions {
+
+const yesNoOptions: Array<IOptions> = [
+  { key: 'Y', value: 'Yes' },
+  { key: 'N', value: 'No' }
+];
+
+const yesNoDont_KnownOptions: Array<IOptions> = [
+  { key: 'Y', value: 'Yes' },
+  { key: 'N', value: 'No' },
+  { key: 'U', value: "Don't Know" },
+];
+
+
+
+interface IOptions {
   key: string;
   value: string;
 } 
 
-export interface IQuestionInfo {
+interface IQuestionInfo {
   key: string;
-  text: string;
   choice: string;
   options: Array<IOptions>;
   show: boolean;
   rules?: any;
+  saveHiddenValue?: boolean;
 }
 
 @Component({
@@ -20,14 +34,47 @@ export interface IQuestionInfo {
   styleUrls: [ './multi-question.component.css' ]
 })
 export class MultiQuestionComponent implements OnInit  {
-  name = 'Angular ' + VERSION.major;
-
-  @Input()
-  public questions: Array<IQuestionInfo> = [];
-
-  @Input()
+  public name = 'Angular ' + VERSION.major;
+  public dwellingType = '';
+  public questions: Array<IQuestionInfo> = [
+    {
+      key: '1', choice: '', options: yesNoOptions, show: true,
+      rules: {always: true}
+    },
+    {
+      key: '2', choice: '', options: yesNoDont_KnownOptions, show: true,
+      rules: {
+        dependsOn: ['1'], // only show this question if the following are visible
+        choiceRules: {
+          '1': ['N'],  // only show if question '3' has a value of 'N'
+        },
+      }
+    },
+    {
+      key: '3', choice: '', options: yesNoOptions, show: true,
+      rules: {
+        dependsOn: ['2'], // only show this question if the following are visible
+        choiceRules: {
+          '2': ['Y', 'U'],  // only show if question '3' has a value of 'N'
+        },
+      }
+    },
+    {
+      key: '4',
+      choice: '', 
+      options: yesNoOptions,
+      rules: {
+        dependsOn: ['3'], // only show this question if the following are visible
+        choiceRules: {
+          '3': ['N'],  // only show if question '3' has a value of 'N'
+        },
+      },
+      saveHiddenValue: true,
+      show: true
+    }
+  ];
   public saveHiddenValues: boolean = false;
-
+  
   constructor() {
   }
 
@@ -37,8 +84,11 @@ export class MultiQuestionComponent implements OnInit  {
 
   onChange(){
     this.evaluateRules();
+
+    // save to backend
   }
 
+  //Helper function to lookup the question by the key
   private getQuestionInfoByKey(key: string): IQuestionInfo|null {
     for(let question of this.questions) {
       if(key === question.key) {
@@ -47,19 +97,23 @@ export class MultiQuestionComponent implements OnInit  {
     }
     return null;
   } 
+  
 
+  //Function which evaluates wheather or not a question is shown
   private evaluateRules() {
+    
+    //Loop over each question
     this.questions.forEach((question: IQuestionInfo) => {
       if('rules' in question) {
         const rules = question.rules;
-
+        //If 'always', evaluate first
         if('always' in rules) {
           question.show = rules.always;
           return;
         }
 
         let show: boolean = true;
-
+        //Default to show questions is true, unless 'dependsOn' makes it false
         if ('dependsOn' in rules) {
           const dependsOn: Array<string> = rules.dependsOn;
 
@@ -68,7 +122,7 @@ export class MultiQuestionComponent implements OnInit  {
               // TODO - an obvious error add behaivor for depends on itself
               continue;  // for right now just ignore
             }
-            const dependsOnQuestionInfo: IQuestionInfo|null = this.getQuestionInfoByKey(key);
+            const dependsOnQuestionInfo = this.getQuestionInfoByKey(key);
             if(dependsOnQuestionInfo) {
               if(dependsOnQuestionInfo.show == false) {
                 show = false;
@@ -98,12 +152,18 @@ export class MultiQuestionComponent implements OnInit  {
             }
           }
 
+          //Clear values of questions not shown
           question.show = show;
-          if(!this.saveHiddenValues && show === false) {
+          const saveHidden = this.saveHiddenValues || question.saveHiddenValue;
+          if(!saveHidden && show === false) {
             question.choice = '';
           }
         }
         }
     });
+
+    // set the dwellingType here
+    const  isHouse = false; // 
+    this.dwellingType = (isHouse)?'house':'appartment';
   }
 }
